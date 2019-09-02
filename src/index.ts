75df44b698dfd96d9ed351d8e3d1b76719085a2a
聂海锋 <niexiaofei@yahoo.com>
2019-09-03 00:26:49 +0800

    feat: 修改index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import Koa from 'koa';
 import * as http from 'http';
 import chalk from 'chalk';
 
-import { IP } from './utils';
-
 const app = new Koa();
 
 app.use(async (ctx: any) => {
@@ -14,10 +12,6 @@ app.on('error', (err: any) => {
   console.log(chalk.bgBlue('server error', err));
 });
 
-// app.listen(3000, () => {
-
-// });
-
 const server = http.createServer(app.callback());
 
 server.listen(3000);
@@ -26,7 +20,6 @@ server.on('listening', () => {
   console.log(`server running on
     ${chalk.blue.bold(`
       Local: http://localhost:3000
-      IP: http://${IP}:3000
     `)}
   `);
 });
